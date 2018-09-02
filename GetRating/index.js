7057c086ba7d3788dd9c1a43d9cb5cedbb26977c
Maysam Mokarian <mamokari@microsoft.com>
2018-09-02 17:22:56 -0400

    added getRating

--- GetRating/index.js
+++ GetRating/index.js
@@ -1,17 +1,26 @@
-module.exports = function (context, req) {
+var dao = require('./dao.js');
+
+module.exports =async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    if (req.query && req.query.id ) {
+        ratings =await  dao.getRating(req.query.id);
+           if (typeof ratings != 'undefined'){
+             context.res = {
+             body: ratings
+       };
+   }else{
+    context.res = {
+        status: 400,
+        body: "id is not valid"
+    };
+   }
+   }
+   else {
+       context.res = {
+           status: 400,
+           body: "id is not provided "
+       };
+   }
     context.done();
 };
